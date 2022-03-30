7b683273258ef79653e9702aee80714a719622f1
Arisito <99370264+Arisito@users.noreply.github.com>
2022-03-30 19:12:54 -0400

    Ejercicio Cola FIFO

---
+++ Ejercicio Cola FIFO.py
@@ -0,0 +1,10 @@
+#Ejemplo de cola - FIFO
+Cola = []
+#Incluir elementos - push - apilar
+Cola.append('c')
+Cola.append('a')
+Cola.append('b')
+print(Cola)
+#Sacar elementos - pop - desapilar
+x=Cola.pop(0)
+print(Cola) #lo restante
